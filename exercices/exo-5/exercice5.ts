04bb9d5dac1eb5b51a6d2fdb88ac6867832986eb
maddaj <j.maddalena2@gmail.com>
2020-04-08 16:12:30 +0200

    Update exo 5

--- exercices/exo-5/exercice5.ts
+++ exercices/exo-5/exercice5.ts
@@ -1,35 +1,35 @@
 class Places{
   artist: string;
-  placeNumber: string ="";
+  price: number = -1;
   date: string;
   location: string;
   name: string;
 
-  constructor(artist: string, location: string, name: string, date: string,  placeNumber ?: string){
+  constructor(artist: string, location: string, name: string, date: string,  price?: number){
     this.artist = artist;
     this.location = location;
     this.name = name;
     this.date = date;
 
-    if(placeNumber){
-      this.placeNumber = placeNumber;
+    if(price){
+      this.price = price;
     }
   }
 
-  htmlDetails(): number{
-    let htmlContent = `
+  htmlDetails(): string{
+    let htmlContent: string = `
     <p>Ville : ${ this.location }</p>
     <p>Artiste : ${ this.artist }</p>
     <p>Date : ${ this.date }</p>
-    <p>Nom : ${ this.name }</p>``;
-    if (this.placeNumber != ""){
-      htmlContent = htmlContent + "Nom du titulaire : ${ this.placeNumber }";
-    };
+    <p>Nom : ${ this.name }</p>`;
+    if (this.price != -1){
+      htmlContent = htmlContent + `Prix de place : ${this.price}`;
+    }
     return htmlContent;
   }
 }
 
 let Places1: Places =  new Places("pierre", "angele", "21.03", "marseille" );
-let Places2: Places =  new Places("pierre", "angele", "21.03", "marseille", "123E" );
+let Places2: Places =  new Places("pierre", "angele", "21.03", "marseille", 12 );
 console.log(Places1.htmlDetails());
 console.log(Places2.htmlDetails());
